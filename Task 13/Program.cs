bb4be4986a0c19c1fd1a6e09e91f80a50f1b56e1
Anton <polyncev1991@mail.ru>
2023-02-09 21:48:37 +0300

    13 Task complete

--- Task 13/Program.cs
+++ Task 13/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+   numbers[i] = new Random().Next(0, 100);
+   Console.Write(" " + Method(i) + " ");
+}
+Console.Write("]");
+
+int Method(int a)
+{
+   return numbers[a];
+}
