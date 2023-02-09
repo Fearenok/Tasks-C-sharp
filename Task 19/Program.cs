0971fd978d31df8849ca00b3c8977bce1cf7136c
Anton <polyncev1991@mail.ru>
2023-02-09 22:08:31 +0300

    19 Task complete

--- Task 19/Program.cs
+++ Task 19/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[,] a = new double[3, 4];
+
+Random random = new Random();
+for (int i = 0; i < 3; i++)
+{
+   for (int j = 0; j < 4; j++)
+   {
+      a[i, j] = random.NextDouble() * 10;
+      Console.Write("{0,6:F2}", a[i, j]);
+   }
+   Console.WriteLine();
+}
