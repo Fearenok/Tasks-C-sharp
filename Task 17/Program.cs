5cd7479d73daf30f2facf918e9e4c049a2d8b7d5
Anton <polyncev1991@mail.ru>
2023-02-09 22:04:46 +0300

    17 Task complete

--- Task 17/Program.cs
+++ Task 17/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число М(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+   if (arr[i] > 0)
+   {
+      count++;
+   }
+}
+
+Console.WriteLine($"Кол-во чисел > 0: {count}");
